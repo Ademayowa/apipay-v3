25e0aba87c3d5ddec0a84d2216c4ac6411c5c860
Ademayowa <ademayowaadeniyi@gmail.com>
2022-07-06 08:05:56 +0100

    testing out aboutsectiontitle components while deploying

--- section/BreadCrumb.js
+++ section/BreadCrumb.js
@@ -1,7 +1,7 @@
 export default function BreadCrumb({ title }) {
   return (
     <>
-      <h2 className='text-[32px] md:text-[56px] text-grayColor px-10 md:px-20 max-w-3xl leading-none mt-10'>
+      <h2 className='text-[56px] md:text-[56px] text-grayColor px-10 md:px-20 max-w-3xl leading-none mt-10'>
         {title}
       </h2>
     </>
